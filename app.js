9d4c746e1effc3c6ff17000afa6995284a0f75b4
Tushar Kandpal <kandpal.tushar@gmail.com>
2021-09-26 19:04:57 +0530

    done

---
+++ app.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkBtn = document.querySelector("#check-btn");
+const output = document.querySelector("#output");
+
+function checkIfBirthdayLucky(sum, luckyNumberValue) {
+  if (sum && luckyNumberValue) {
+    if (sum % luckyNumberValue === 0)
+      output.innerText = "Yaay! your birthday is lucky 🥳";
+    else output.innerText = "Nope, just you are 😉";
+  } else output.innerText = "Please enter valid values 🙄!";
+}
+
+function getDobSum(dob) {
+  dobValue = dob.replaceAll("-", "");
+  // console.log(dobValue);
+
+  let sum = 0;
+
+  for (let digit of dobValue) {
+    sum = sum + Number(digit);
+  }
+
+  return sum;
+}
+
+function getValues() {
+  let dob = dateOfBirth.value;
+  let luckyNumberValue = luckyNumber.value;
+
+  let sum = getDobSum(dob);
+  console.log(luckyNumberValue);
+  checkIfBirthdayLucky(sum, luckyNumberValue);
+}
+
+checkBtn.addEventListener("click", getValues);
